47a67d3c9d2df28f30e68fc6a68406b9680a916e
Dario Nieddu <cy.rae@yahoo.com>
2024-07-05 11:58:14 +0200

    inital commit

---
+++ database_core/src/main/java/com/fh_ma/database_core/DatabaseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fh_ma.database_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseCoreApplication.class, args);
+	}
+
+}
---
+++ database_core/src/test/java/com/fh_ma/database_core/DatabaseCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh_ma.database_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
